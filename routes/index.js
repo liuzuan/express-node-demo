b4429dff9f570f7b7ab446f374968343ca692986
liuzuan <467209099@qq.com>
2018-05-03 23:03:41 +0800

    start

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
+
 module.exports = router;
